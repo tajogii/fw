c6f66ee8c63e4f95fe1cd0dffe6c18ca500bab51
tajogii <tajogi@mail.ru>
2023-12-23 13:49:44 +0500

    full init

--- backend/auth/src/index.ts
+++ backend/auth/src/index.ts
@@ -1,8 +1,17 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import swaggerUi from 'swagger-ui-express';
+import * as swaggerDocument from '../swagger/openapi.json';
 
-const authapp = express();
-const authport = 5000;
+dotenv.config()
+const authapp = express()
+const authport = process.env.AUTHPORT
+
+authapp.use('/api-doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
 authapp.get('/', (request, response) => {
-  response.send('Hello world!');
+    response.send('Hello world!')
 });
-authapp.listen(authport, () => console.log(`Running on port ${authport}`));
+
+
+authapp.listen(authport, () => console.log(`Auth module run port:${authport}`))
